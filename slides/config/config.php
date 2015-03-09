9677d0bf29123eacd02cadaa8503359129099bd3
Jirka Korejtko <korejtko@avast.com>
2015-03-09 23:30:04 +0100

    Fixed path in config

--- slides/config/config.php
+++ slides/config/config.php
@@ -7,7 +7,7 @@
  * To change this template use File | Settings | File Templates.
  */
 session_start();
-require_once("libs/dibi.min.php");
+require_once("../libs/dibi.min.php");
 require_once("db_config.php");
 
 $conference_date = "November 23, 2013";
